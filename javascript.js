0863f68ed7371755df3c1a1271b3af6217090603
jcampbell <jcampbell57@gmail.com>
2022-07-03 17:44:05 -0700

    added indie-flower font, markers, and fixed board and font sizes

--- javascript.js
+++ javascript.js
@@ -3,31 +3,35 @@ const gameboard = (function () {
     const gameContainer = document.querySelector('.gameContainer');
     const gameboardArray = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
 
-    for (let i = 1; i<10; i++) {
+    // for (let i = 0; i<9; i++) {
+    //     let newCell = document.createElement('div');
+    //     newCell.id=`cell${i}`
+    //     gameContainer.appendChild(newCell);
+    // }
+
+    for (item in gameboardArray) {
         let newCell = document.createElement('div');
-        newCell.id=`cell${i}`
+        newCell.id= item;
+        console.log(gameboardArray[item]);
+        newCell.textContent = gameboardArray[item];
         gameContainer.appendChild(newCell);
     }
 
     // player objects
     playerA = {
-        // marker: marker,
-        // score: score,
+        marker: 'X',
+        wins: 0,
     }
     
     playerB = {
-        // marker: marker,
-        // score: score,
+        marker: 'o',
+        wins: 0,
     }
     
     // game 
     game = {
-        render: function () {
-            return gameContainer.innerHTML = `${gameboardArray}`
-        }
+
     }
 
-    return gameboardArray;
-    return game;
 
 })();
