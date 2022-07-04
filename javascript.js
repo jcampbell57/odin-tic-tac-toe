4b684b611ed68f997219b663dbeb6dbfe4dc0306
jcampbell <jcampbell57@gmail.com>
2022-07-03 20:03:15 -0700

    added ability to place marks and reset board

--- javascript.js
+++ javascript.js
@@ -1,37 +1,67 @@
 
 const gameboard = (function () {
     const gameContainer = document.querySelector('.gameContainer');
-    const gameboardArray = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
-
-    // for (let i = 0; i<9; i++) {
-    //     let newCell = document.createElement('div');
-    //     newCell.id=`cell${i}`
-    //     gameContainer.appendChild(newCell);
-    // }
-
-    for (item in gameboardArray) {
-        let newCell = document.createElement('div');
-        newCell.id= item;
-        console.log(gameboardArray[item]);
-        newCell.textContent = gameboardArray[item];
-        gameContainer.appendChild(newCell);
-    }
+    const reset = document.querySelector('.reset');
+    // const marks = ['X', '', '', '', 'X', '', 'X', '', 'X']
 
     // player objects
     playerA = {
         marker: 'X',
         wins: 0,
-    }
+    };
     
     playerB = {
-        marker: 'o',
+        marker: 'O',
         wins: 0,
-    }
+    };
     
     // game 
     game = {
+        marks: ['X', '', '', '', 'X', '', 'X', '', 'X'],
 
+        render: function () {
+
+            // clear board
+            let cellCount = gameContainer.childElementCount + 1;
+            for (let i=1; i<cellCount; i++) {
+                gameContainer.children[0].remove();
+            }
+
+            // populate board
+            for (item in game.marks) {
+                let newCell = document.createElement('div');
+                newCell.classList.add('cell')
+                newCell.id = item;
+                // console.log(game.marks[item]);
+                newCell.textContent = game.marks[item];
+                gameContainer.appendChild(newCell);
+            }        
+        },
+
+        reset: function () {
+            game.marks = ['', '', '', '', '', '', '', '', ''];
+            game.render();
+        },
+
+        // placeMark: function (e) {
+        //     }
     }
 
+    //listeners 
+    // user input listener
+    gameContainer.addEventListener('click', (e) => {
+        if (game.marks[e.target.id] !== '') {
+            console.log('error')
+            console.log(e.target.id)
+
+        } else {
+            game.marks[e.target.id] = 'O';
+            game.render();
+        }
+    });
+    // game reset listener
+    reset.addEventListener('click', game.reset);
+
+    game.render();
 
 })();
