83663a267d6ce60f72ead9244eaaa17fc1b7072c
jcampbell57 <jcampbell57@gmail.com>
2024-03-23 00:10:51 -0700

    start javascript file

---
+++ v2/javascript.js
@@ -0,0 +1,25 @@
+(function() {
+  function gameboard() {
+    let squares = ['', '', '', '', '', '', '', '', ''];
+    
+    return { };
+  };
+
+  function player(marker) {
+    return { marker };
+  };
+
+  const game = (function() {
+    const playerX = player('X')
+    const playerO = player('O')
+    let currentPlayer = playerX
+
+
+
+    const init = () => {
+
+    }
+  })();
+
+  game.init();
+})();
